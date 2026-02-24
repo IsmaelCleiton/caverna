b8b6d41497a3b24b88b63673d82ebea8f5675de2
IsmaelCleiton <ismaelcleitonsf@gmail.com>
2026-02-23 22:24:21 -0300

    feat: add @astrojs/mdx dependency and create test blog post
    style: update sidebar and layout colors to #333
    
    fix: remove unused CSS link in create post page
    
    fix: update social links in 'sobre' page with actual URLs
    
    chore: format tsconfig.json for better readability

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  integrations: [mdx(),]
 });
---
+++ src/content/blog/teste-code.md
@@ -0,0 +1,11 @@
+---
+title: 'teste code'
+description: 'asd'
+pubDate: '2026-02-23T13:02:17.895Z'
+author: 'Ismael Cleiton'
+slug: 'teste-code'
+---
+
+> ```
+> Vamos testar isso
+> ```
